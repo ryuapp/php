d9ffeb3560d57c5054689a8bf09573c9348973ad
ryu <114303361+ryuapp@users.noreply.github.com>
2024-02-10 11:54:49 +0900

    📝 Adjust JSDoc in tan()

--- math/tan.ts
+++ math/tan.ts
@@ -1,9 +1,9 @@
 /**
- * Returns the tan of num in radians.\
+ * Returns the tangent of num in radians.\
  * The num parameter is in radians.
  *
  * @param {number} num An angle in radians
- * @return {number} The tan of num in radians
+ * @return {number} The tangent of num in radians
  */
 export function tan(num: number): number {
   return Math.tan(num);
